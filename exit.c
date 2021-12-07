3730b3b8e276309f5e9f598fcd06157080c7f579
Freek Bes <freekbladnl@gmail.com>
2021-12-07 19:28:34 +0100

    finally, exit on the same line on Ctrl+D

--- exit.c
+++ exit.c
@@ -6,7 +6,7 @@
 /*   By: jgalloni <jgalloni@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/26 23:45:59 by jgalloni      #+#    #+#                 */
-/*   Updated: 2021/12/06 20:27:38 by jgalloni      ########   odam.nl         */
+/*   Updated: 2021/12/07 19:28:14 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 #include "readline/readline.h"
 #include "custom_errors.h"
 
-/*
- * @brief Handle signals 
- * SIGINT is ctrl c
- * -1 is passed in case of EOF
- */
+/**
+  * @brief Handle signals
+  * SIGINT is ctrl c
+  * -1 is passed in case of EOF
+  */
 
 void	sig_handler(int sig)
 {
@@ -39,28 +39,37 @@ void	sig_handler(int sig)
 	else if (sig == SIGINT)
 	{
 		printf("\n"); // Move to a new line
-    	rl_on_new_line(); // Regenerate the prompt on a newline
-    	rl_replace_line("", 0); // Clear the previous text
-    	rl_redisplay();
+		rl_replace_line("", 0); // Clear the previous text
+		rl_on_new_line(); // Regenerate the prompt on a newline
+		rl_redisplay();
 	}
 }
 
-/*
- * @brief Prints a custom or errno message and exits the process
- */
+/**
+  * @brief Prints a custom or errno message and exits the process
+  * @param err An error code. If set to -2, nothing is printed.
+  */
 
 void	exit_shell_w_error(int err)
 {
 	if (err == -1)
 	{
-		printf(" exit\n");
+		printf("\x1b[1A\033[12C\bexit\n");
+		rl_replace_line("exit", 1);
 		exit(errno);
 	}
-	if (err == CMDNF)
+	else if (err == -2)
+	{
+		exit(errno);
+	}
+	else if (err == CMDNF)
 	{
 		printf("minishell: command not found\n");
 		exit (CMDNF);
 	}
-	perror("minishell");
-	exit(errno);
+	else
+	{
+		perror("minishell");
+		exit(errno);
+	}
 }
