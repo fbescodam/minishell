a77ff784c9107a237fd9ecbccac0d7b355d636d3
Freek Bes <freekbladnl@gmail.com>
2021-12-07 19:53:15 +0100

    fix cd without arguments

--- reserved_cmds.c
+++ reserved_cmds.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/05 17:50:19 by fbes          #+#    #+#                 */
-/*   Updated: 2021/12/07 18:27:38 by fbes          ########   odam.nl         */
+/*   Updated: 2021/12/07 19:52:43 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,12 @@
 int	check_run_reserved_cmds(t_cmd *cmd)
 {
 	if (ft_strncmp("cd\0", cmd->params[0], 3) == 0)
-		return (chdir(cmd->params[1]));
+	{
+		if (cmd->params[1])
+			return (chdir(cmd->params[1]));
+		else
+			return (chdir(getenv("HOME")));
+	}
 	if (ft_strncmp("exit\0", cmd->params[0], 5) == 0)
 		exit_shell_w_error(-2);
 	return (1);
