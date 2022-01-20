bda0be92e59bd3a8656707d90fc965c5a014892f
Freek Bes <freekbladnl@gmail.com>
2022-01-20 19:17:28 +0100

    add comment

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/19 22:47:09 by fbes          #+#    #+#                 */
-/*   Updated: 2022/01/20 18:09:29 by fbes          ########   odam.nl         */
+/*   Updated: 2022/01/20 19:17:15 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int	mini_cd(t_cmd *cmd)
 {
+	// add check to see if path exists!
 	if (cmd->params[1])
 		return (chdir(cmd->params[1]));
 	else
