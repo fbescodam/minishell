c6ea54e5325129191315fe46ab6ec4e04bc1698d
Freek Bes <freekbladnl@gmail.com>
2022-01-20 19:18:43 +0100

    added builtin env method

--- builtins/env.c
+++ builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/20 18:11:32 by fbes          #+#    #+#                 */
-/*   Updated: 2022/01/20 18:21:14 by fbes          ########   odam.nl         */
+/*   Updated: 2022/01/20 19:17:37 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,14 @@
 
 int	mini_env(t_cmd *cmd)
 {
-	// TODO
-	printf("todo\n");
+	t_list	*temp;
+
+	temp = cmd->mini->envars;
+	while (temp)
+	{
+		printf("%s=%s\n", ((t_envar *)temp->content)->name,
+			((t_envar *)temp->content)->val);
+		temp = temp->next;
+	}
 	return (0);
 }
