bb67d1fbc9d224a2baf765b1f42ebefce00163c2
Freek Bes <freekbladnl@gmail.com>
2022-01-25 23:29:57 +0100

    added forgotten function to Makefile and added comment

--- libft/ft_strclen.c
+++ libft/ft_strclen.c
@@ -6,12 +6,18 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 19:24:26 by fbes          #+#    #+#                 */
-/*   Updated: 2021/06/14 21:45:54 by freek         ########   odam.nl         */
+/*   Updated: 2022/01/25 23:27:40 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * Find the length of a string until a certain character (or EOF)
+ * @param s The string to get the length of
+ * @param c The character to end counting at
+ * @return The length of string *s
+ */
 size_t	ft_strclen(const char *s, char c)
 {
 	size_t	i;
