91131c4f90495333aab4069b63713e99d5b2d237
jollajj98 <joelle.galloni98@gmail.com>
2022-01-29 17:35:32 +0100

    Progress in parsing.. skipping spaces and quotes attempt

--- includes/parse.h
+++ includes/parse.h
@@ -1,7 +1,7 @@
 #ifndef PARSE_H
 # define PARSE_H
 
-int		scan_operators(char *prompt, char *operators);
+int		scan_operators(char *prompt, char *operators, int quote_is_operator);
 char	*skip_chars(char *prompt, char *mask_set);
 int		parse_prompt(t_mini *mini, char *prompt);
 int		add_string_to_array(char ***to, char *from);
