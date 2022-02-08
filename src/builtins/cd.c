ca66bfd951ca5fc14d464bec8e9b4bf7fe821d2f
Freek Bes <freekbladnl@gmail.com>
2022-02-08 18:06:42 +0100

    change PWD envar on builtin cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,23 +1,38 @@
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "utils.h"
 #include "envars.h"
 #include "custom_errors.h"
-#include <errno.h>
 
 int	mini_cd(t_cmd *cmd)
 {
-	t_envar	*home;
+	t_envar	*envar;
+	char	*path;
 	int		ret;
 
 	if (cmd->params[1])
-		ret = chdir(cmd->params[1]);
+		path = cmd->params[1];
 	else
 	{
-		home = get_envar(cmd->mini->envars, "HOME");
-		if (!home)
+		envar = get_envar(cmd->mini->envars, "HOME");
+		if (!envar)
 			exit_shell_w_error(cmd, ERR_ENV_NOT_SET);
-		ret = chdir(home->val);
+		path = envar->val;
 	}
-	if (ret < 0)
+	if (chdir(path) < 0)
 		return (errno);
+	path = getcwd(NULL, 0);
+	if (!path)
+		return (ENOMEM);
+	envar = get_envar(cmd->mini->envars, "PWD");
+	if (!envar && !set_envar(cmd->mini, "PWD", path, 0))
+	{
+		free(path);
+		return (ENOMEM);
+	}
+	if (!replace_envar_value(envar, path))
+		return (ENOMEM);
+	free(path);
 	return (0);
 }
