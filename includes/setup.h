983244a63822d2d4f589b37e91f4d8c1410dc155
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-02-28 17:04:11 +0100

    \x1b to \e

--- includes/setup.h
+++ includes/setup.h
@@ -3,7 +3,7 @@
 
 # include "structs.h"
 
-# define KUT "\001\x1b[1;40;31m\002hell\001\x1b[0m\x1b[7;40;31m\002shell\001\x1b[0m\x1b[1m\002> \001\x1b[0m\002"
+# define KUT "\001\e[1;40;31m\002hell\001\e[0m\e[7;40;31m\002shell\001\e[0m\e[1m\002> \001\e[0m\002"
 
 int			setup_mini(t_mini *mini, char **envp);
 int			setup_signals(t_mini *mini);
