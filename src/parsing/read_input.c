324e3a924f8d8175fffa17006ecf0f3948bc9271
jollajj98 <joelle.galloni98@gmail.com>
2022-03-03 16:48:59 +0100

    Now it's prettier

--- src/parsing/read_input.c
+++ src/parsing/read_input.c
@@ -12,7 +12,7 @@ int	read_til_close_pipe(char ***to)
 	ret = 0;
 	while (1)
 	{
-		in = readline("\001\e[1;40;31m\002>\001\e[0m\e[7;40;31m\002open_pipe\001\e[0m\e[1m\002> \001\e[0m\002");
+		in = readline("\001\e[1;40;31m\002>\001\e[0m\002 ");
 		if (in[0])
 			break;
 		free(in);
