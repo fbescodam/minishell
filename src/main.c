a552784f99bb946baff1b94f4cc04266b65e29de
Freek Bes <freekbladnl@gmail.com>
2022-04-08 23:18:50 +0200

    norminette-proofed main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/27 17:21:02 by fbes          #+#    #+#                 */
-/*   Updated: 2022/04/08 22:41:23 by fbes          ########   odam.nl         */
+/*   Updated: 2022/04/08 23:18:27 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,40 @@
 #include "utils.h"
 #include "execute.h"
 
+void	after_rl(t_mini *mini)
+{
+	int		ret;
+
+	add_history(mini->prompt);
+	ret = parse_set_envars_b4_comm(mini, &mini->prompt);
+	if (ret != 0)
+		error_manager(mini, ret);
+	else
+	{
+		if (ret != 0)
+			error_manager(mini, ret);
+		else
+		{
+			if (!set_mini_status(mini, 0))
+				error_manager(mini, ENOMEM);
+			ret = parse_prompt(mini, mini->prompt);
+			if (ret != 0)
+				error_manager(mini, ret);
+			else
+			{
+				ret = execute_list(mini);
+				if (ret != 0)
+					error_manager(mini, ret);
+			}
+		}
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_mini	mini;
-	int		ret;
 
-	//rl_catch_signals = 0;
+	argc = 3;
 	if (!setup_mini(&mini, argv[0], envp))
 		force_exit(&mini, 1);
 	while (1)
@@ -42,31 +70,7 @@ int	main(int argc, char **argv, char **envp)
 			rl_replace_line(" exit", 1);
 			force_exit(&mini, 0);
 		}
-		add_history(mini.prompt);
-		//printf("prompt after readline: \"%s\"\n", mini.prompt);
-		ret = parse_set_envars_b4_comm(&mini, &mini.prompt);
-		if (ret != 0)
-			error_manager(&mini, ret);
-		else
-		{
-			if (ret != 0)
-				error_manager(&mini, ret);
-			else
-			{
-				if (!set_mini_status(&mini, 0))
-					error_manager(&mini, ENOMEM);
-				//printf("prompt after parsing envars: \"%s\"\n", mini.prompt);
-				ret = parse_prompt(&mini, mini.prompt);
-				if (ret != 0)
-					error_manager(&mini, ret);
-				else
-				{
-					//print_command_list(mini.cmds);
-					ret = execute_list(&mini);
-				}
-				//printf("prompt after parse_prompt: \"%s\"\n", mini.prompt);
-			}
-		}
+		after_rl(&mini);
 		ft_free(mini.prompt);
 		ft_lstclear(&(mini.cmds), &free_cmd);
 		mini.prompt = NULL;
