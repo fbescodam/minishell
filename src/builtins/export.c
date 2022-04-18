1236500832036bd6cf66eecab28d909760b5cb6d
Freek Bes <freekbladnl@gmail.com>
2022-04-18 17:25:42 +0200

    fixed export cmd printing errors twice

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/27 17:21:57 by fbes          #+#    #+#                 */
-/*   Updated: 2022/04/14 23:18:36 by fbes          ########   odam.nl         */
+/*   Updated: 2022/04/18 17:24:59 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ static void	export_err(char *id)
 	print_builtin_err("export", id, "not a valid identifier");
 }
 
-static int	export_only(int is_child, size_t i, t_cmd *cmd)
+static int	export_only(size_t i, t_cmd *cmd)
 {
 	t_envar	*envar;
 
-	if (!is_valid_env_name(cmd->params[i]) && is_child)
-		export_err(cmd->params[i]);
 	envar = get_envar(cmd->mini->envars, cmd->params[i]);
 	if (envar)
 	{
@@ -38,16 +36,11 @@ static int	export_only(int is_child, size_t i, t_cmd *cmd)
 	return (1);
 }
 
-static int	set_and_export(int is_child, int i, t_cmd *cmd, char *equals_pos)
+static int	set_and_export(int i, t_cmd *cmd, char *equals_pos)
 {
-	if (!is_valid_env_name(cmd->params[i]) && is_child)
-		export_err(cmd->params[i]);
-	else
-	{
-		*equals_pos = '\0';
-		if (!set_envar(cmd->mini, cmd->params[i], equals_pos + 1, 1))
-			return (0);
-	}
+	*equals_pos = '\0';
+	if (!set_envar(cmd->mini, cmd->params[i], equals_pos + 1, 1))
+		return (0);
 	return (1);
 }
 
@@ -64,9 +57,11 @@ int	mini_export(int is_child, t_cmd *cmd)
 		equals_pos = ft_strchr(cmd->params[i], '=');
 		if (equals_pos == cmd->params[i] && is_child)
 			export_err(cmd->params[i]);
-		else if (equals_pos && !set_and_export(is_child, i, cmd, equals_pos))
+		else if (!is_valid_env_name(cmd->params[i]) && is_child)
+			export_err(cmd->params[i]);
+		else if (equals_pos && !set_and_export(i, cmd, equals_pos))
 			return (ENOMEM);
-		else if (!export_only(is_child, i, cmd))
+		else if (!export_only(i, cmd))
 			return (ENOMEM);
 		i++;
 	}
